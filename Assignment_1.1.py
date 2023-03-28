a74d55e01ecc39de79c86750ca8c6333e1496e86
Naveen Padimi <122008488+naveenpadimi@users.noreply.github.com>
2023-03-28 16:36:19 +0530

    Add files via upload

---
+++ Assignment_1.1.py
@@ -0,0 +1,85 @@
+# Data Types:
+
+#1. What is a data type in Python?
+
+#   A data type is a classification of data which tells the interpreter or compiler how to treat a piece of data.
+
+# 2.How many built-in data types does Python have?
+
+#    Python has six built-in data types: integer, float, boolean, string, list, and tuple.
+
+# 3.What is the difference between mutable and immutable data types in Python?
+
+#   Mutable data types can be changed after they are created, while immutable data types cannot be changed.
+# 4.Can you give an example of a mutable and an immutable data type in Python?
+
+#   A list is an example of a mutable data type, while a string is an example of an immutable data type.
+
+# Operators:
+
+#5. What are operators in Python?
+
+#   Operators are symbols or keywords that perform operations on operands.
+
+# 6.What is the difference between arithmetic and logical operators?
+
+#    Arithmetic operators perform mathematical operations, while logical operators perform boolean operations.
+
+# 7.What is the use of the comparison operator in Python?
+
+#   The comparison operator is used to compare two values and return a boolean value.
+
+# 8.Can you give an example of the ternary operator in Python?
+
+#   The ternary operator in Python is a shorthand way of writing an if-else statement. An example is: x = 10 if y > 5 else 20.
+# Conditional Statements:
+
+#9. What are conditional statements in Python?
+
+#   Conditional statements are statements that perform different actions based on whether a condition is true or false.
+
+# 10.What is the difference between the if and elif statements?
+
+#   The if statement is used to perform an action if a condition is true, while the elif statement is used to perform an action if the previous condition is false and the current condition is true.
+
+#11. How do you use the and and or operators in conditional statements?
+
+#   The and operator returns True if both conditions are true, while the or operator returns True if at least one condition is true.
+
+# 12.Can you give an example of nested conditional statements in Python?
+
+# An example of nested conditional statements is: if x > 10: if y > 5: print("x is greater than 10 and y is greater than 5").
+
+# Looping Statements:
+
+#13. What are looping statements in Python?
+
+#    Looping statements are statements that repeat a set of instructions until a condition is met.
+
+# 14.What is the difference between a for loop and a while loop?
+
+#    A for loop is used to iterate over a sequence of items, while a while loop is used to repeat a set of instructions while a condition is true.
+
+# 15.How do you break out of a loop in Python?
+
+# You can break out of a loop in Python using the break keyword.
+
+# 16.Can you give an example of a nested loop in Python?
+
+#   An example of a nested loop in Python is: for i in range(5): for j in range(i): print(j, end=" ") print()
+# Functions:
+
+# 17.What is a function in Python?
+
+#   A function is a block of code that performs a specific task.
+
+# 18.How do you define a function in Python?
+
+#   You can define a function in Python using the def keyword.
+
+# 19.What is the difference between a return and a print statement in a function?
+
+#    A return statement returns a value from a function, while a print statement outputs a value to the console.
+# 20.Can you give an example of a recursive function in Python?
+
+#   An example of a recursive function in Python is: def factorial(n): if n == 0: return 1 else: return n * factorial(n-1)
